567578cf19d908eb6e4438b6d8667c2ebe3df8be
marukun_ <87289638+marukun712@users.noreply.github.com>
2022-08-25 10:12:39 +0900

    whitelist

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,7 @@ const port = process.env.PORT || 8080;
 import * as cors_proxy from 'cors-anywhere';
 cors_proxy.createServer({
     // 許可するオリジンを記載
-    originWhitelist: ['https://example.com'],
-    requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
+    originWhitelist: ['*'],
 }).listen(port, host, function () {
     console.log('OHTAM CORS Anywhere起動中 ' + host + ':' + port);
 });
