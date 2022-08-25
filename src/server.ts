865cde7e7ccd9b7bce661a7eb35ecdcb7eedbcde
marukun_ <87289638+marukun712@users.noreply.github.com>
2022-08-25 09:08:38 +0900

    first

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+const host = process.env.HOST || '0.0.0.0';
+const port = process.env.PORT || 8080;
+
+import * as cors_proxy from 'cors-anywhere';
+cors_proxy.createServer({
+    // 許可するオリジンを記載
+    originWhitelist: ['https://example.com'],
+    requireHeader: ['origin', 'x-requested-with'],
+    removeHeaders: ['cookie', 'cookie2']
+}).listen(port, host, function () {
+    console.log('OHTAM CORS Anywhere起動中 ' + host + ':' + port);
+});
